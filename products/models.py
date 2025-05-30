3dc56fa5a54f1020f5d3d6c4c7b47bee36c1776f
Aswin <ashwinraj9072a@gmail.com>
2025-05-30 12:11:46 +0530

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Models for products
+
+class Product(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = ((LIVE, 'Live'),(DELETE, 'Delete'),)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.FloatField()
+    image = models.ImageField(upload_to='products/')
+    priority = models.IntegerField(default=0)
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title       #string representation of the product
