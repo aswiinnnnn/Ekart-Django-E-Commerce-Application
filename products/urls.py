f55797a34c3d7cabed7d26c6b921000caba2248f
Aswin <ashwinraj9072a@gmail.com>
2025-06-06 16:25:08 +0530

    till home page updated

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
    path('', views.index, name='index'),
    path('products/', views.list_products, name='list_products'),
-   path('detail_product/', views.detail_products, name='detail_products'),
+   path('detail_product/<pk>/', views.detail_products, name='detail_products'),
 ]
--- products/views.py
+++ products/views.py
@@ -4,7 +4,14 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    freatured_products = roducts = Product.objects.order_by("-priority")[:8]  # Fetch the top 8 products based on priority
+    latest_products = roducts = Product.objects.order_by("-id")[:8]  # Fetch the latest 8 products
+    context = {
+        'freatured_products': freatured_products,
+        'latest_products': latest_products
+    }
+
+    return render(request, 'index.html', context)
 
 
 
@@ -15,15 +22,22 @@ def list_products(request):
     if request.GET:
         page = request.GET.get('page', 1)
 
-    products = Product.objects.all()  # Fetch all products from the database
-    product_paginator = Paginator(products, 4)  # Show 10 products per page
+    products = Product.objects.order_by("-priority")  # Fetch all products from the database
+    product_paginator = Paginator(products, 8)  # Show 10 products per page
     product_list = product_paginator.get_page(page)
     context = {
         'products': product_list
     }
     return render(request, 'products.html', context)
 
-def detail_products(request):
+def detail_products(request,pk):
+
+    product_detail = Product.objects.get(pk=pk)  # Fetch the product by primary key
+    context = {
+        'product': product_detail
+    }
     # Here you would typically fetch the product from the database using the product_id
     # For now, we'll just render a placeholder template
-    return render(request, 'product_detail.html')
+
+
+    return render(request, 'product_detail.html', context)
